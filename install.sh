778d48d079d0d75fc7f996755bcc10ad01d04b55
Dmitriy Korobko <dLEAM@yandex.ru>
2022-09-08 10:47:26 +0300

    Fixed Downloads directory

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ satisfy_requirements() {
         read -p "Requirements for installation is not satisfied. Do you want to install needed packages?(Y/n): " ans
         if [ $ans = "yes" ] || [ $ans = "y" ] || [ $ans = "Y" ]
         then
-            sudo pacman -S lolcat figlet gum 
+            sudo pacman -S lolcat figlet gum --noconfirm
         else
             echo
             echo "Installation aborted :("
@@ -55,14 +55,28 @@ install_large_packages () {
     yay -S paper-icon-theme
 }
 
+menu () {
+    gum style --border normal --margin "1" --padding "1 2" --border-foreground 75 " Welcome to $(gum style --foreground 77 'installation') of my beautiful dotfiles for ArchLinux! "
+    echo " Please select the installation steps (select on $(gum style --foreground 77 "space")): "
 
-hardware_install () {
+    DEFAULT_INSTALL="Default installation (for notebooks)"
+    PATCH_PC="Remove battery and brightness icons from bar (For PC and VM)"
+    PATCH_VM="Add environment variable for alacritty (For VM)"
+    ACTIONS=$(gum choose --selected.foreground=77 --cursor.foreground=77 --cursor-prefix "[ ] " --selected-prefix "[✓] " --no-limit "$DEFAULT_INSTALL" "$PATCH_PC" "$PATCH_VM")
+    echo $ACTIONS
+    grep -q "$DEFAULT_INSTALL"<<< "$ACTIONS" && gum style --bold --foreground=76 "Running default installation" -- sleep 1;install
+    grep -q "$PATCH_PC" <<< "$ACTIONS" && gum style --bold --foreground=76 "Patching bar" -- sleep 1;
+    grep -q "$PATCH_VM" <<< "$ACTIONS" && gum style --bold --foreground=76 "Patching alacritty config" -- sleep 5;
+}
+
+
+install () {
     # Adding directories
     mkdir -p "$HOME/.config"
     mkdir -p "$HOME/.bin"
     mkdir -p "$HOME/.fonts"
     mkdir -p "$HOME/Pictures/Screenshots"        
-    mkdir -p "$HOME/Dowloads"
+    mkdir -p "$HOME/Downloads"
     mkdir -p "$HOME/Projects/from_source"
     install_yay
     yay -Syyuu -< pkgs.lst
@@ -79,7 +93,7 @@ hardware_install () {
 }
 
 
-
-satisfy_requirements
-logo
-hardware_install
+menu
+#satisfy_requirements
+#logo
+#install
